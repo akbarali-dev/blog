cb20086b1f5a1e9ef5117fc01ab9ab186d9c398f
akbarali-dev <akbaraliasqaraliyev0610@gmail.com>
2023-10-05 14:17:13 +0500

    create goals model

--- blog/models/goals.py
+++ blog/models/goals.py
@@ -2,4 +2,16 @@ from django.contrib.auth import get_user_model
 from .base_model import BaseModel
 from django.db import models
 
+from . import User
+
 DjangoUser = get_user_model()
+
+
+class Goals(BaseModel, models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='goals')
+    title = models.CharField(max_length=125)
+    description = models.TextField()
+    icon = models.TextField()
+
+    class Meta:
+        db_table = 'goals'
