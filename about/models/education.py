01a8556980cfd05f485666b7a85aaa639d50df7f
akbarali-dev <akbaraliasqaraliyev0610@gmail.com>
2023-10-06 15:58:08 +0500

    created module education.py

--- about/models/education.py
+++ about/models/education.py
@@ -1,5 +1,14 @@
-from django.contrib.auth import get_user_model
+from blog.models import User
 from blog.models.base_model import BaseModel
 from django.db import models
 
-DjangoUser = get_user_model()
+
+class Education(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='education')
+    name = models.CharField(max_length=100)
+    start_year = models.IntegerField()
+    graduation_year = models.IntegerField()
+    description = models.TextField()
+
+    class Meta:
+        db_table = 'education'
