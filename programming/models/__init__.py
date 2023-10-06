80759a580784c84bc133975b2bc4913ae3d42fd8
akbarali-dev <akbaraliasqaraliyev0610@gmail.com>
2023-10-06 11:26:49 +0500

    technology module created

--- programming/models/__init__.py
+++ programming/models/__init__.py
@@ -1,1 +1,3 @@
 from .category import Category
+from .skills import Skills
+from .techalogy import Technology
--- programming/models/techalogy.py
+++ programming/models/techalogy.py
@@ -1,5 +1,16 @@
 from django.contrib.auth import get_user_model
-from blog.models.base_model import BaseModel
+
+from blog.models import User
+from blog.models.base_model import BaseModel, IntegerRangeField
 from django.db import models
 
 DjangoUser = get_user_model()
+
+
+class Technology(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='skills')
+    name = models.CharField(max_length=100)
+    discount = IntegerRangeField(max_value=100, min_value=0)
+
+    class Meta:
+        db_table = 'technalogy'
