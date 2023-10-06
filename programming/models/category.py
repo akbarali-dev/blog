a5638672c6c1bd17bac142b13ef7271c5f41ebb8
akbarali-dev <akbaraliasqaraliyev0610@gmail.com>
2023-10-06 11:03:00 +0500

    category module created

--- programming/models/category.py
+++ programming/models/category.py
@@ -3,3 +3,10 @@ from blog.models.base_model import BaseModel
 from django.db import models
 
 DjangoUser = get_user_model()
+
+
+class Category(BaseModel):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'category'
