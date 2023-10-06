1b7ba691e0f69eacc98cb0ae1d8a98fea5919980
akbarali-dev <akbaraliasqaraliyev0610@gmail.com>
2023-10-06 11:26:18 +0500

    skills module created

--- programming/models/skills.py
+++ programming/models/skills.py
@@ -1,5 +1,18 @@
 from django.contrib.auth import get_user_model
-from blog.models.base_model import BaseModel
+
+from blog.models import User
+from blog.models.base_model import BaseModel, IntegerRangeField
 from django.db import models
 
+from programming.models.techalogy import Technology
+
 DjangoUser = get_user_model()
+
+
+class Skills(BaseModel):
+    name = models.CharField(max_length=100)
+    discount = IntegerRangeField(max_value=100, min_value=0)
+    technology = models.ForeignKey(Technology, on_delete=models.CASCADE, related_name='skills')
+
+    class Meta:
+        db_table = 'skills'
