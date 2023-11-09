e4057d3d6aff51d9888a696d1e39dd647100a6eb
akbarali-dev <akbaraliasqaraliyev0610@gmail.com>
2023-11-09 16:15:29 +0500

    add allowed host

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6bormqlyn45tgjsmz#*54)gdc5=d8fqyv8mocyf7@*h$3s@+w#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["http://localhost:5000", "*", "back.javthon.uz"]
+ALLOWED_HOSTS = ["http://localhost:5000", "*", "back.javthon.uz", "0.0.0.0", "localhost"]
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
