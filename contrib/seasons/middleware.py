c1e5dbf3a73913ac516e148fb46c933e26a146e4
akbarali-dev <akbaraliasqaraliyev0610@gmail.com>
2023-11-09 18:41:26 +0500

    change ip address saver

--- contrib/seasons/middleware.py
+++ contrib/seasons/middleware.py
@@ -6,10 +6,25 @@ class VisitorMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        ip_address = request.META.get('REMOTE_ADDR')
-        referring_url = request.META.get('HTTP_REFERER')
-        print("data: ", request.path)
-        # if request.method == "POST" and "HTTP_REFERER" in request.META:
-        Visitor.objects.create(ip_address=ip_address, referring_url=request.path)
+        ip_address = self.get_client_ip(request)
+        Visitor.objects.create(ip_address=ip_address)
         response = self.get_response(request)
         return response
+
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
+
+
+    # def __call__(self, request):
+    #     ip_address = request.META.get('REMOTE_ADDR')
+    #     referring_url = request.META.get('HTTP_REFERER')
+    #     print("data: ", request.path)
+    #     # if request.method == "POST" and "HTTP_REFERER" in request.META:
+    #     Visitor.objects.create(ip_address=ip_address, referring_url=request.path)
+    #     response = self.get_response(request)
+    #     return response
