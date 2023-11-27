ef7f1dd23de4d058c601240c16546588d0f04589
akbarali-dev <akbaraliasqaraliyev0610@gmail.com>
2023-11-27 15:09:43 +0500

    change visitor middleware.py send bot new user

--- contrib/seasons/middleware.py
+++ contrib/seasons/middleware.py
@@ -1,8 +1,25 @@
 import os
+from multiprocessing.sharedctypes import synchronized
 
 from blog.models import Visitor
 import requests
 from urllib.parse import quote
+
+
+def already_ip_address(ip_address):
+    print("hello")
+    if not Visitor.objects.filter(ip_address=ip_address).exists():
+        response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+        message = "#new_user\n"
+        message += "IP address: " + ip_address
+        message += "\nCity: " + str(response.get("city"))
+        message += "\nRegion: " + str(response.get("region"))
+        message += "\nCountry: " + str(response.get("country_name"))
+
+        token = os.environ.get("BOT_TOKEN")
+        encoded_message = quote(message)
+        requests.get("https://api.telegram.org/bot" + token + "/sendMessage?chat_id"
+                                                              "=1474104201&text=" + encoded_message + "")
 
 
 class VisitorMiddleware:
@@ -11,7 +28,7 @@ class VisitorMiddleware:
 
     def __call__(self, request):
         ip_address = self.get_client_ip(request)
-
+        already_ip_address(ip_address=ip_address)
         if request.path.startswith('/admin/'):
             Visitor.objects.create(ip_address=ip_address, referring_url=request.path, category='A')
         elif request.path.startswith('/static/'):
@@ -23,21 +40,9 @@ class VisitorMiddleware:
             Visitor.objects.create(ip_address=ip_address, referring_url=request.path, category='W')
         response = self.get_response(request)
 
-        if not Visitor.objects.filter(ip_address=ip_address).exists():
-            response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
-            message = "#new_user\n"
-            message += "IP address: " + ip_address
-            message += "\nCity: " + str(response.get("city"))
-            message += "\nRegion: " + str(response.get("region"))
-            message += "\nCountry: " + str(response.get("country_name"))
-
-            token = os.environ.get("BOT_TOKEN")
-            encoded_message = quote(message)
-            requests.get("https://api.telegram.org/bot" + token + "/sendMessage?chat_id"
-                                                                  "=1474104201&text=" + encoded_message + "")
-
         return response
 
+    # @staticmethod
     def get_client_ip(self, request):
         x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
         if x_forwarded_for:
